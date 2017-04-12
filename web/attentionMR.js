5038b5010a0da6b3b791bb85b46851daeb16f25f
Matīss <matiss@lielakeda.lv>
2017-04-12 23:55:58 +0300

    Tab refactoring

--- web/attentionMR.js
+++ web/attentionMR.js
@@ -54,37 +54,37 @@
 			);
 			return ret;
 		}
-                function calculatePosition1(a, s, e, b, m,d1){
-                        var total=d3.sum(a);
-                        var sum=0, neededHeight=0, leftoverHeight= e-s-2*b*a.length;
-                        var ret =[];
-                        a.forEach(
-                                function(d){
-                                        var v={};
-                                        v.percent = (total == 0 ? 0 : d/total);
-                                        v.value=d;
-                                        v.height=Math.max(v.percent*(e-s-2*b*a.length), m);
-                                        (v.height==m ? leftoverHeight-=m : neededHeight+=v.height );
-                                        ret.push(v);
-                                }
-                        );
+		function calculatePosition1(a, s, e, b, m,d1){
+			var total=d3.sum(a);
+			var sum=0, neededHeight=0, leftoverHeight= e-s-2*b*a.length;
+			var ret =[];
+			a.forEach(
+				function(d){
+					var v={};
+					v.percent = (total == 0 ? 0 : d/total);
+					v.value=d;
+					v.height=Math.max(v.percent*(e-s-2*b*a.length), m);
+					(v.height==m ? leftoverHeight-=m : neededHeight+=v.height );
+					ret.push(v);
+				}
+			);
 
-                        var scaleFact=leftoverHeight/Math.max(neededHeight,1), sum=0;
+			var scaleFact=leftoverHeight/Math.max(neededHeight,1), sum=0;
 
-                        ret.forEach(
-                                function(d){
-                                        d.percent = scaleFact*d.percent;
-                                        d.height=(d.height==m? m : d.height*scaleFact);
-                                        d.middle=sum+b+d.height/2;
-                                        d.y=s + d.middle - d.percent*(e-s-2*b*a.length)/2;
-                                        d.h= d.percent*(e-s-2*b*a.length);
-                                        d.percent = (total == 0 ? 0 : d.value/total);
-                                        sum+=2*b+d.height;
+			ret.forEach(
+				function(d){
+					d.percent = scaleFact*d.percent;
+					d.height=(d.height==m? m : d.height*scaleFact);
+					d.middle=sum+b+d.height/2;
+					d.y=s + d.middle - d.percent*(e-s-2*b*a.length)/2;
+					d.h= d.percent*(e-s-2*b*a.length);
+					d.percent = (total == 0 ? 0 : d.value/total);
+					sum+=2*b+d.height;
 					d.p = d1;
-                                }
-                        );
-                        return ret;
-                }
+				}
+			);
+			return ret;
+		}
 		vis.mainBars = [ 
 			calculatePosition1( data.data[0].map(function(d){ return d3.sum(d);}), 0, height, buffMargin, minHeight,0),
 			calculatePosition1( data.data[1].map(function(d){ return d3.sum(d);}), 0, height, buffMargin, minHeight,1)
@@ -161,15 +161,15 @@
 		d3.select("#"+id).append("g").attr("class","edges").attr("transform","translate("+ b+",0)");
 
 		d3.select("#"+id).select(".edges").selectAll(".edge")
-                         .data(data.edges).enter().append("line").attr("class","edge")
+			.data(data.edges).enter().append("line").attr("class","edge")
 			.attr("x1", function(d) {return d.y1;})     // x position of the first end of the line
-                        .attr("y1", function(d) {return d.h1;})      // y position of the first end of the line
-                        .attr("x2", function(d) {return d.y2;})     // x position of the second end of the line
-                        .attr("y2", function(d) {return d.h2;})
-                        .style("stroke-width", function(d) { //console.log(d); 
-							return d.va * 2; 
-						})//function(d) { //console.log(d); return d.value * 2; })
-                        .style("stroke", function(d) {return color(d.va);});
+			.attr("y1", function(d) {return d.h1;})      // y position of the first end of the line
+			.attr("x2", function(d) {return d.y2;})     // x position of the second end of the line
+			.attr("y2", function(d) {return d.h2;})
+			.style("stroke-width", function(d) { //console.log(d); 
+				return d.va * 2; 
+			})//function(d) { //console.log(d); return d.value * 2; })
+			.style("stroke", function(d) {return color(d.va);});
 	}	
 	
 	function drawHeader(header, id){
@@ -288,4 +288,3 @@
 	
 	this.bP = bP;
 }();
-
