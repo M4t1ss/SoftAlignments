3c091d2369d3b5442b8e65882a6ec3881f382bef
Matīss Rikters <matiss_rikters@hotmail.com>
2017-06-22 11:16:31 +0300

    Added a test to compare color/block/block2 output

---
+++ test_data/tests/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR=test_data/tests
+
+set -ex
+
+DIFF1=`comm -2 -3 <(sort $DIR/weHope.txt) <(sort $DIR/weHope-test.txt) | wc -c`
+
+if [ $DIFF1 == 0 ] ; then
+	echo "Tests OK."
+else
+	echo "Test 1 Filed."
+fi
+
+rm -rf $DIR/weHope-test.txt
