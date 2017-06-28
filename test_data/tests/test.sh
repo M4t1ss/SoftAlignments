4d8dfb020e86e4547c54d9408dac3deacf421360
Matīss Rikters <matiss_rikters@hotmail.com>
2017-06-28 11:12:37 +0300

    Fixed the test

--- test_data/tests/test.sh
+++ test_data/tests/test.sh
@@ -3,7 +3,7 @@ DIR=test_data/tests
 
 set -ex
 
-DIFF1=`comm -2 -3 <(sort $DIR/weHope.txt) <(sort $DIR/weHope-test.txt) | wc -c`
+DIFF1=`comm -3 <(sort $DIR/weHope.txt) <(sort $DIR/weHope-test.txt) | wc -c`
 
 if [ $DIFF1 == 0 ] ; then
 	echo "Tests OK."
