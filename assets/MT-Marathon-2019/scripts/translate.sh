940703b5581fb51fac3df2b0e8c511003adedda8
Matīss Rikters <matiss_rikters@hotmail.com>
2019-08-25 10:07:00 +0900

    updates

--- assets/MT-Marathon-2019/scripts/translate.sh
+++ assets/MT-Marathon-2019/scripts/translate.sh
@@ -2,8 +2,34 @@
 
 PATH_TO_MARIAN=
 
-$PATH_TO_MARIAN/marian-decoder -c model.npz.best-translation.npz.decoder.yml --alignment soft --quiet-translation < newstest2019.piece.lt > newstest2019.translation.alignments.en
-$PATH_TO_MARIAN/marian-decoder -c model.npz.best-translation.npz.decoder.yml --alignment soft --quiet-translation < songs.piece.lt > songs.translation.alignments.en
+$PATH_TO_MARIAN/marian-decoder \
+	-c transformer-model.npz.best-translation.npz.decoder.yml \
+	--alignment soft \
+	--quiet-translation \
+	< newstest2019.piece.lt \
+	> newstest2019.transformer.alignments.en
+$PATH_TO_MARIAN/marian-decoder \
+	-c transformer-model.npz.best-translation.npz.decoder.yml \
+	--alignment soft \
+	--quiet-translation \
+	< songs.piece.lt \
+	> songs.transformer.alignments.en
 
-python format-output.py newstest2019.translation.alignments.en newstest2019.piece.lt > newstest2019.translation.alignments.formatted.en
-python format-output.py songs.translation.alignments.en songs.piece.lt > songs.translation.alignments.formatted.en
+$PATH_TO_MARIAN/marian-decoder \
+	-c rnn-model.npz.best-translation.npz.decoder.yml \
+	--alignment soft \
+	--quiet-translation \
+	< newstest2019.piece.lt \
+	> newstest2019.rnn.alignments.en
+$PATH_TO_MARIAN/marian-decoder \
+	-c rnn-model.npz.best-translation.npz.decoder.yml \
+	--alignment soft \
+	--quiet-translation \
+	< songs.piece.lt \
+	> songs.rnn.alignments.en
+
+python format-output.py newstest2019.transformer.alignments.en newstest2019.piece.lt > newstest2019.transformer.alignments.formatted.en
+python format-output.py songs.transformer.alignments.en songs.piece.lt > songs.transformer.alignments.formatted.en
+
+python format-output.py newstest2019.rnn.alignments.en newstest2019.piece.lt > newstest2019.rnn.alignments.formatted.en
+python format-output.py songs.rnn.alignments.en songs.piece.lt > songs.rnn.alignments.formatted.en
