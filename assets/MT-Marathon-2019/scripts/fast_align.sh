8f6a8b1bac8a86833941fbe5730768849f693ddf
Matīss Rikters <matiss_rikters@hotmail.com>
2021-08-06 19:30:36 +0900

    Added a data filtering step for fast_align

--- assets/MT-Marathon-2019/scripts/fast_align.sh
+++ assets/MT-Marathon-2019/scripts/fast_align.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 . ./data.sh
 
+# Concatenate source & target
 $MOSES_DIR/scripts/ems/support/prepare-fast-align.perl $PREFIX.$EXP_SRC $PREFIX.$EXP_TRG > $PREFIX.$EXP_SRC-$EXP_TRG
 	
+# Clean up empty lines
+cat $PREFIX.$EXP_SRC-$EXP_TRG | grep -v -e "||| $" | grep -v -e "^ |||" > c.$PREFIX.$EXP_SRC-$EXP_TRG
+cat c.$PREFIX.$EXP_SRC-$EXP_TRG | cut -d '|' -f1 | awk '{$1=$1};1' > c.$PREFIX.$EXP_SRC
+cat c.$PREFIX.$EXP_SRC-$EXP_TRG | cut -d '|' -f4 | awk '{$1=$1};1' > c.$PREFIX.$EXP_TRG
+	
+# Do the magic
+$FAST_ALIGN_DIR/fast_align -i c.$PREFIX.$EXP_SRC-$EXP_TRG -d -o -v > c.$PREFIX.$EXP_SRC-$EXP_TRG.forward.align
+$FAST_ALIGN_DIR/fast_align -i c.$PREFIX.$EXP_SRC-$EXP_TRG -d -o -v -r > c.$PREFIX.$EXP_SRC-$EXP_TRG.reverse.align
 
-$FAST_ALIGN_DIR/fast_align -i $PREFIX.$EXP_SRC-$EXP_TRG -d -o -v > $PREFIX.$EXP_SRC-$EXP_TRG.forward.align
-$FAST_ALIGN_DIR/fast_align -i $PREFIX.$EXP_SRC-$EXP_TRG -d -o -v -r > $PREFIX.$EXP_SRC-$EXP_TRG.reverse.align
+$FAST_ALIGN_DIR/atools -i c.$PREFIX.$EXP_SRC-$EXP_TRG.forward.align -j c.$PREFIX.$EXP_SRC-$EXP_TRG.reverse.align -c grow-diag-final-and > c.$PREFIX.$EXP_SRC-$EXP_TRG.grow-diag-final-and
 
-$FAST_ALIGN_DIR/atools -i $PREFIX.$EXP_SRC-$EXP_TRG.forward.align -j $PREFIX.$EXP_SRC-$EXP_TRG.reverse.align -c grow-diag-final-and > $PREFIX.$EXP_SRC-$EXP_TRG.grow-diag-final-and
